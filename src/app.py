7055dbf403ff27f3d8d5aaf429a988c221ddef19
dancrg <jdcespedesrojas@gmail.com>
2025-07-21 20:20:25 +0000

    Entrega Proyecto SQL

--- src/app.py
+++ src/app.py
@@ -1,12 +1,4 @@
-
-# PREVIAMENTE:
-
-# a) He instalado las librerias necesarias para el actual proyecto, contenidas en el archivo 'requeriments.txt'
-# b) Ya he instalado PostgreSQL y la interfaz pgAdmin en mi computador
-# c) He creado mi database y usuario con contrasena mediante pgAdmin 4, no con la consola
-# d) El archivo .env ha sido editado con la informacion anterior, el DB_HOST y DB_PORT
-
-
+# PREVIAMENTE se llevo a cabo los pasos del archivo 'INSTRUCTIONS.es.md'
 
 
 
@@ -15,8 +7,8 @@
 import os                                       # Para utilizar funciones de mi sistema operativo
 import pandas as pd
 from sqlalchemy import create_engine, text      # Para conectarme a mi base de datos:
-                                                # crete_engige me permite crear una conexion (motor) a mi base de datos
-                                                # text me permite escribir en lenguaje SQL como texto        
+                                                    # crete_engige me permite crear una conexion (motor) a mi base de datos
+                                                    # text me permite escribir en lenguaje SQL como texto        
 from dotenv import load_dotenv                  # Para usar mi archivo .env
 
 
@@ -69,18 +61,19 @@ if engine is None:
 # Creo las tablas indicadas en el archivo 'create.sql'
 # Usamos engine.connect()
 # Aca haremos uso de text de sqlalchemy
+# Debido a error presentado despues de ejecutado por 1ra vez el script, se anade IF NO EXIST a cada tabla
 
 
 with engine.connect() as connection:
     connection.execute(text("""
                             
-    CREATE TABLE publishers(
+    CREATE TABLE IF NOT EXISTS publishers(
         publisher_id INT NOT NULL,
         name VARCHAR(255) NOT NULL,
         PRIMARY KEY(publisher_id)
     );
 
-    CREATE TABLE authors(
+    CREATE TABLE IF NOT EXISTS authors(
         author_id INT NOT NULL,
         first_name VARCHAR(100) NOT NULL,
         middle_name VARCHAR(50) NULL,
@@ -88,7 +81,7 @@ with engine.connect() as connection:
         PRIMARY KEY(author_id)
     );
 
-    CREATE TABLE books(
+    CREATE TABLE IF NOT EXISTS books(
         book_id INT NOT NULL,
         title VARCHAR(255) NOT NULL,
         total_pages INT NULL,
@@ -100,7 +93,7 @@ with engine.connect() as connection:
         CONSTRAINT fk_publisher FOREIGN KEY(publisher_id) REFERENCES publishers(publisher_id)
     );
 
-    CREATE TABLE book_authors (
+    CREATE TABLE IF NOT EXISTS book_authors (
         book_id INT NOT NULL,
         author_id INT NOT NULL,
         PRIMARY KEY(book_id, author_id),
@@ -115,51 +108,53 @@ with engine.connect() as connection:
 
 # 3) Insert data
 
-# Inserto los datos para cada tabla, indicados en el archivo 'insert.sql'
+# Inserto los registros para cada tabla, indicados en el archivo 'insert.sql'
 # Usamos nuevamente engine.connect()
 # Aca nuevamente haremos uso de text de sqlalchemy
+# Debido a error presentado despues de ejecutado por 1ra vez el script, se anade ON CONFLICT DO NOTHING a cada registro
+
 
 with engine.connect() as connection:
     connection.execute(text("""
                             
-    INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media');
-    INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart');
-    INSERT INTO publishers(publisher_id, name) VALUES (3, 'A K PETERS');
-    INSERT INTO publishers(publisher_id, name) VALUES (4, 'Academic Press');
-    INSERT INTO publishers(publisher_id, name) VALUES (5, 'Addison Wesley');
-    INSERT INTO publishers(publisher_id, name) VALUES (6, 'Albert&Sweigart');
-    INSERT INTO publishers(publisher_id, name) VALUES (7, 'Alfred A. Knopf');
+    INSERT INTO publishers(publisher_id, name) VALUES (1, 'O Reilly Media') ON CONFLICT DO NOTHING;
+    INSERT INTO publishers(publisher_id, name) VALUES (2, 'A Book Apart') ON CONFLICT DO NOTHING;
+    INSERT INTO publishers(publisher_id, name) VALUES (3, 'A K PETERS') ON CONFLICT DO NOTHING;
+    INSERT INTO publishers(publisher_id, name) VALUES (4, 'Academic Press') ON CONFLICT DO NOTHING;
+    INSERT INTO publishers(publisher_id, name) VALUES (5, 'Addison Wesley') ON CONFLICT DO NOTHING;
+    INSERT INTO publishers(publisher_id, name) VALUES (6, 'Albert&Sweigart') ON CONFLICT DO NOTHING;
+    INSERT INTO publishers(publisher_id, name) VALUES (7, 'Alfred A. Knopf') ON CONFLICT DO NOTHING;
     
-    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (1, 'Merritt', null, 'Eric');
-    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (2, 'Linda', null, 'Mui');
-    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (3, 'Alecos', null, 'Papadatos');
-    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (4, 'Anthony', null, 'Molinaro');
-    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (5, 'David', null, 'Cronin');
-    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (6, 'Richard', null, 'Blum');
-    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (7, 'Yuval', 'Noah', 'Harari');
-    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (8, 'Paul', null, 'Albitz');
+    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (1, 'Merritt', null, 'Eric') ON CONFLICT DO NOTHING;
+    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (2, 'Linda', null, 'Mui') ON CONFLICT DO NOTHING;
+    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (3, 'Alecos', null, 'Papadatos') ON CONFLICT DO NOTHING;
+    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (4, 'Anthony', null, 'Molinaro') ON CONFLICT DO NOTHING;
+    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (5, 'David', null, 'Cronin') ON CONFLICT DO NOTHING;
+    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (6, 'Richard', null, 'Blum') ON CONFLICT DO NOTHING;
+    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (7, 'Yuval', 'Noah', 'Harari') ON CONFLICT DO NOTHING;
+    INSERT INTO authors (author_id, first_name, middle_name, last_name) VALUES (8, 'Paul', null, 'Albitz') ON CONFLICT DO NOTHING;
 
-    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (1, 'Lean Software Development: An Agile Toolkit', 240, 4.17, '9780320000000', '2003-05-18', 5);
-    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (2, 'Facing the Intelligence Explosion', 91, 3.87, null, '2013-02-01', 7);
-    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (3, 'Scala in Action', 419, 3.74, '9781940000000', '2013-04-10', 1);
-    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (4, 'Patterns of Software: Tales from the Software Community', 256, 3.84, '9780200000000', '1996-08-15', 1);
-    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (5, 'Anatomy Of LISP', 446, 4.43, '9780070000000', '1978-01-01', 3);
-    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (6, 'Computing machinery and intelligence', 24, 4.17, null, '2009-03-22', 4);
-    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (7, 'XML: Visual QuickStart Guide', 269, 3.66, '9780320000000', '2009-01-01', 5);
-    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (8, 'SQL Cookbook', 595, 3.95, '9780600000000', '2005-12-01', 7);
-    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (9, 'The Apollo Guidance Computer: Architecture And Operation (Springer Praxis Books / Space Exploration)', 439, 4.29, '9781440000000', '2010-07-01', 6);
-    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (10, 'Minds and Computers: An Introduction to the Philosophy of Artificial Intelligence', 222, 3.54, '9780750000000', '2007-02-13', 7);
+    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (1, 'Lean Software Development: An Agile Toolkit', 240, 4.17, '9780320000000', '2003-05-18', 5) ON CONFLICT DO NOTHING;
+    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (2, 'Facing the Intelligence Explosion', 91, 3.87, null, '2013-02-01', 7) ON CONFLICT DO NOTHING;
+    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (3, 'Scala in Action', 419, 3.74, '9781940000000', '2013-04-10', 1) ON CONFLICT DO NOTHING;
+    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (4, 'Patterns of Software: Tales from the Software Community', 256, 3.84, '9780200000000', '1996-08-15', 1) ON CONFLICT DO NOTHING;
+    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (5, 'Anatomy Of LISP', 446, 4.43, '9780070000000', '1978-01-01', 3) ON CONFLICT DO NOTHING;
+    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (6, 'Computing machinery and intelligence', 24, 4.17, null, '2009-03-22', 4) ON CONFLICT DO NOTHING;
+    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (7, 'XML: Visual QuickStart Guide', 269, 3.66, '9780320000000', '2009-01-01', 5) ON CONFLICT DO NOTHING;
+    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (8, 'SQL Cookbook', 595, 3.95, '9780600000000', '2005-12-01', 7) ON CONFLICT DO NOTHING;
+    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (9, 'The Apollo Guidance Computer: Architecture And Operation (Springer Praxis Books / Space Exploration)', 439, 4.29, '9781440000000', '2010-07-01', 6) ON CONFLICT DO NOTHING;
+    INSERT INTO books (book_id, title, total_pages, rating, isbn, published_date, publisher_id) VALUES (10, 'Minds and Computers: An Introduction to the Philosophy of Artificial Intelligence', 222, 3.54, '9780750000000', '2007-02-13', 7) ON CONFLICT DO NOTHING;
 
-    INSERT INTO book_authors (book_id, author_id) VALUES (1, 1);
-    INSERT INTO book_authors (book_id, author_id) VALUES (2, 8);
-    INSERT INTO book_authors (book_id, author_id) VALUES (3, 7);
-    INSERT INTO book_authors (book_id, author_id) VALUES (4, 6);
-    INSERT INTO book_authors (book_id, author_id) VALUES (5, 5);
-    INSERT INTO book_authors (book_id, author_id) VALUES (6, 4);
-    INSERT INTO book_authors (book_id, author_id) VALUES (7, 3);
-    INSERT INTO book_authors (book_id, author_id) VALUES (8, 2);
-    INSERT INTO book_authors (book_id, author_id) VALUES (9, 4);
-    INSERT INTO book_authors (book_id, author_id) VALUES (10, 1);
+    INSERT INTO book_authors (book_id, author_id) VALUES (1, 1) ON CONFLICT DO NOTHING;
+    INSERT INTO book_authors (book_id, author_id) VALUES (2, 8) ON CONFLICT DO NOTHING;
+    INSERT INTO book_authors (book_id, author_id) VALUES (3, 7) ON CONFLICT DO NOTHING;
+    INSERT INTO book_authors (book_id, author_id) VALUES (4, 6) ON CONFLICT DO NOTHING;
+    INSERT INTO book_authors (book_id, author_id) VALUES (5, 5) ON CONFLICT DO NOTHING;
+    INSERT INTO book_authors (book_id, author_id) VALUES (6, 4) ON CONFLICT DO NOTHING;
+    INSERT INTO book_authors (book_id, author_id) VALUES (7, 3) ON CONFLICT DO NOTHING;
+    INSERT INTO book_authors (book_id, author_id) VALUES (8, 2) ON CONFLICT DO NOTHING;
+    INSERT INTO book_authors (book_id, author_id) VALUES (9, 4) ON CONFLICT DO NOTHING;
+    INSERT INTO book_authors (book_id, author_id) VALUES (10, 1) ON CONFLICT DO NOTHING;
     """))
 
 
@@ -169,5 +164,18 @@ with engine.connect() as connection:
 
 # Imprimo (leo) las 4 tablas creadas con sus datos insertados
 
-df = pd.read_sql("SELECT * FROM publishers;", engine)
-print(df)
+df1 = pd.read_sql("SELECT * FROM publishers;", engine)
+print(df1)
+
+df2 = pd.read_sql("SELECT * FROM authors;", engine)
+print(df2)
+
+df3 = pd.read_sql("SELECT * FROM books;", engine)
+print(df3)
+
+df4 = pd.read_sql("SELECT * FROM book_authors;", engine)
+print(df4)
+
+
+# Se ejecuta el actual script de python desde la consola respectiva e imprime las tablas correctamente
+# En la consola de SQL se verifica mediante \d que a la base de datos le fueron anadidas posee dichas tablas
